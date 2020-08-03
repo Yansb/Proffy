761ee6c3329a93cadc1e031f0de8cca1288e4def
Yan Santana <yansbarreiro@gmail.com>
2020-08-03 19:19:23 -0300

    :sparkles: Header

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+
+function TeacherList(){
+  return (
+    <div id="page-teacher-list">
+      <PageHeader title="Estes são os proffys disponíveis." >
+        teste
+      </PageHeader>
+    </div>
+  )
+}
+
+export default TeacherList;
