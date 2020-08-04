d14baca5860845beef53796e86259e02703cd5ef
Yan Santana <yansbarreiro@gmail.com>
2020-08-04 19:35:24 -0300

    :card_file_box: creating controllers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('✏️ server online');
+});
