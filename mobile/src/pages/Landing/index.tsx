3de7d756c6eb504123aaf18ef040c30139c6af04
Yan Santana <yansbarreiro@gmail.com>
2020-08-06 15:59:18 -0300

    :iphone: Layout mobile concluido

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing(){
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages(){
+    navigate('Study')
+  }
+
   return(
     <View style={styles.container}>
       <Image style={styles.banner} source={landingImg} />
@@ -25,10 +29,11 @@ function Landing(){
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
       </Text>
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon}/>
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
+        
         <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
           <Image source={giveClassesIcon}/>
           <Text style={styles.buttonText}>Dar aulas</Text>
